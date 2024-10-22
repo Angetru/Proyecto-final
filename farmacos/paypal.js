21ee71b44152c912e008ef5beca8cc1a063b704c
Angetru <162651383+Angetru@users.noreply.github.com>
2024-10-21 23:39:33 -0300

    Delete farmacos directory

--- farmacos/paypal.js
+++
@@ -1,8 +0,0 @@
-const checkoutNodeJssdk = require('@paypal/checkout-server-sdk');
-
-const environment = new checkoutNodeJssdk.core.SandboxEnvironment(
-    process.env.PAYPAL_CLIENT_ID,
-    process.env.PAYPAL_KEY);
-const client = new checkoutNodeJssdk.core.PayPalHttpClient(environment);
-
-module.exports = { client };
